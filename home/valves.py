f0120f6d54599cd042e763b3ec982f9dffb1bea5
Dorian Jaminais-Grellier <dorian@jaminais.fr>
2021-11-09 18:27:40 -0800

    Add WaterMain lock to prevent multiple concurrent usages

--- home/valves.py
+++ home/valves.py
@@ -1,5 +1,6 @@
 import json
 import logging
+from asyncio import Lock
 from datetime import timedelta
 from enum import Enum
 
@@ -10,6 +11,26 @@ from home.time import TimeZone, now, today_at
 from home.utils import n_tries
 
 log = logging.getLogger(__name__)
+
+
+class WaterMain:
+    _lock: Lock = Lock()
+    _users: set[str] = set()
+
+    @classmethod
+    async def start_using(cls: type["WaterMain"], user: str) -> None:
+        async with cls._lock:
+            cls._users.add(user)
+
+    @classmethod
+    async def stop_using(cls: type["WaterMain"], user: str) -> None:
+        async with cls._lock:
+            if user in cls._users:
+                cls._users.remove(user)
+
+    @classmethod
+    async def has_other_users(cls: type["WaterMain"], user: str) -> bool:
+        return cls._users != {user} and cls._users != set()
 
 
 class State(Enum):
@@ -39,6 +60,9 @@ class Valve(Actionable):
         if now() < today_at(*self.not_earlier_than):
             return State.Off
 
+        if await WaterMain.has_other_users(self.area):
+            return State.Off
+
         promql = f'{{ {self.prom_labels}, __name__=~"mqtt_state_l[0-9]", __name__!="{self.prom_metric}" }} == 1'
         other_running = await prom_query_labels(promql)
         if other_running:
@@ -56,35 +80,29 @@ class Valve(Actionable):
         return {0.0: State.Off, 1.0: State.On}[state]
 
     async def apply_state(self: "Valve", state: State) -> None:
+        if state == State.On:
+            await WaterMain.start_using(self.area)
+        else:
+            await WaterMain.stop_using(self.area)
+
         await mqtt_send(
             self.entity.mqtt, json.dumps({f"state_l{self.line}": state.value})
         )
         self.log.info(f"Switched {state.value.lower()}.")
 
 
+_NET = (18, 00, TimeZone.PT)
 VALVE_BACKYARD_SIDE = Valve(
-    "side",
-    line=1,
-    duration=timedelta(minutes=10),
-    not_earlier_than=(20, 00, TimeZone.PT),
+    "side", line=1, duration=timedelta(minutes=10), not_earlier_than=_NET
 )
 VALVE_BACKYARD_HOUSE = Valve(
-    "house",
-    line=2,
-    duration=timedelta(minutes=15),
-    not_earlier_than=(20, 10, TimeZone.PT),
+    "house", line=2, duration=timedelta(minutes=15), not_earlier_than=_NET
 )
 VALVE_BACKYARD_SCHOOL = Valve(
-    "school",
-    line=3,
-    duration=timedelta(minutes=15),
-    not_earlier_than=(20, 25, TimeZone.PT),
+    "school", line=3, duration=timedelta(minutes=10), not_earlier_than=_NET
 )
 VALVE_BACKYARD_DECK = Valve(
-    "deck",
-    line=4,
-    duration=timedelta(minutes=15),
-    not_earlier_than=(20, 40, TimeZone.PT),
+    "deck", line=4, duration=timedelta(minutes=10), not_earlier_than=_NET
 )
 
 ALL_VALVES = [
