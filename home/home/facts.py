770147255a6fb4debe54f1ed95223d76b790bd68
Dorian Jaminais-Grellier <dorian+home@jaminais.fr>
2021-11-29 22:30:10 -0800

    Use movion sensors to estimate day time

--- home/home/facts.py
+++ home/home/facts.py
@@ -4,6 +4,7 @@ from datetime import datetime, timedelta
 
 import aiohttp
 from aioprometheus import Gauge
+from home.prometheus import prom_query_one
 
 from home.time import now
 from home.utils import n_tries
@@ -38,40 +39,9 @@ async def has_other_water_users(user: str) -> bool:
 _PROM_IS_DAY_TIME = Gauge("is_day_time", "1 if day, 0 if night.")
 
 
-@n_tries(3)
 async def is_day_time() -> bool:
-    async def _get_sun_times() -> tuple[datetime, datetime]:
-        try:
-            async with aiohttp.ClientSession() as session:
-                async with session.get(
-                    "https://api.sunrise-sunset.org/json",
-                    params={
-                        "lat": 37.4845223,
-                        "lng": -122.1405406,
-                        "formatted": 0,
-                    },
-                ) as response:
-                    rs = await response.json()
-                    assert rs["status"] == "OK", f"Sunrise/Sunset API failed: {rs}"
-                    sunrise = datetime.fromisoformat(rs["results"]["sunrise"])
-                    sunset = datetime.fromisoformat(rs["results"]["sunset"])
-                    return sunrise, sunset
-        except Exception as err:
-            log.debug(f"is_night_time Failed: {err}")
-            raise
-
-    sunrise, sunset = await _get_sun_times()
-    for offset in (
-        timedelta(days=-1),
-        timedelta(days=0),
-        timedelta(days=1),
-    ):
-        if sunrise < now() + offset < sunset:
-            _PROM_IS_DAY_TIME.set({"city": "east_palo_alto"}, 1)
-            return True
-    _PROM_IS_DAY_TIME.set({"city": "east_palo_alto"}, 0)
-    return False
-
-
-async def is_night_time() -> bool:
-    return not await is_day_time()
+    promql = "max(max_over_time(mqtt_illuminance_lux[1h]))"
+    lux = await prom_query_one(promql)
+    is_day = lux > 100
+    _PROM_IS_DAY_TIME.set({"city": "east_palo_alto"}, is_day)
+    return bool(is_day)
