3ca812ae44923d297578ac72faefd025c83ba4c4
Dorian Jaminais-Grellier <dorian+home@jaminais.fr>
2021-11-30 22:10:19 -0800

    improve reliability of update script

--- update.sh
+++ update.sh
@@ -1,8 +1,9 @@
 set -eux
 
+
 cd /github/home/
 git pull
 
-systemctl stop home
+OLD_PROC=$(docker ps | grep "home:latest" | awk '{print $1}')
 docker build -t home home/
-systemctl start home
+docker stop ${OLD_PROC}  # systemd will restart with the new image.
