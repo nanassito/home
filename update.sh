882e7d2c10b77be78b8dfa90e23a606a429e2e3d
Dorian Jaminais-Grellier <dorian+home@jaminais.fr>
2021-12-04 21:49:19 -0800

    Improve upgrade script

--- update.sh
+++ update.sh
@@ -2,8 +2,26 @@ set -eux
 
 
 cd /github/home/
+
+git_version(){
+    git log -q $1 | head -n1 | awk '{print $2}'
+}
+
+
+HOME_GIT_PREV=$(git_version home)
+PROM_GIT_PREV=$(git_version prometheus)
+
+
 git pull -s recursive -X theirs
 
-OLD_PROC=$(docker ps | grep "home:latest" | awk '{print $1}')
-docker build -t home home/
-docker stop ${OLD_PROC}  # systemd will restart with the new image.
+
+if [ "${HOME_GIT_PREV}" != "$(git_version home)" ]; then
+    OLD_PROC=$(docker ps | grep "home:latest" | awk '{print $1}')
+    docker build -t home home/
+    docker stop ${OLD_PROC}  # systemd will restart with the new image.
+fi
+
+if [ "${PROM_GIT_PREV}" != "$(git_version prometheus)" ]; then
+    systemctl daemon-reload
+    systemctl restart prometheus
+fi
