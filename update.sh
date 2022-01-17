a04d6c8472faedc723ee7b7d6d26e15f5d16f0ed
Dorian Jaminais-Grellier <dorian+home@jaminais.fr>
2022-01-17 12:09:05 -0800

    add support for all services in the update script

--- update.sh
+++ update.sh
@@ -8,13 +8,26 @@ git_version(){
 }
 
 
+ALERTMANAGER_GIT_PREV=$(git_version alertmanager)
+GRAFANA_GIT_PREV=$(git_version grafana)
 HOME_GIT_PREV=$(git_version home)
-PROM_GIT_PREV=$(git_version prometheus)
 NGINX_GIT_PREV=$(git_version nginx)
+PROMETHEUS_GIT_PREV=$(git_version prometheus)
+ZIGBEE2MQTT_GIT_PREV=$(git_version zigbee2mqtt)
 
 
 git pull -s recursive -X theirs
 
+
+if [ "${ALERTMANAGER_GIT_PREV}" != "$(git_version alertmanager)" ]; then
+    systemctl daemon-reload
+    systemctl restart alertmanager
+fi
+
+if [ "${GRAFANA_GIT_PREV}" != "$(git_version grafana)" ]; then
+    systemctl daemon-reload
+    systemctl restart grafana
+fi
 
 if [ "${HOME_GIT_PREV}" != "$(git_version home)" ]; then
     OLD_PROC=$(docker ps | grep "home:latest" | awk '{print $1}')
@@ -22,12 +35,17 @@ if [ "${HOME_GIT_PREV}" != "$(git_version home)" ]; then
     docker stop ${OLD_PROC}  # systemd will restart with the new image.
 fi
 
-if [ "${PROM_GIT_PREV}" != "$(git_version prometheus)" ]; then
+if [ "${NGINX_GIT_PREV}" != "$(git_version nginx)" ]; then
+    systemctl daemon-reload
+    systemctl restart nginx
+fi
+
+if [ "${PROMETHEUS_GIT_PREV}" != "$(git_version prometheus)" ]; then
     systemctl daemon-reload
     systemctl restart prometheus
 fi
 
-if [ "${NGINX_GIT_PREV}" != "$(git_version nginx)" ]; then
+if [ "${ZIGBEE2MQTT_GIT_PREV}" != "$(git_version zigbee2mqtt)" ]; then
     systemctl daemon-reload
-    systemctl restart nginx
+    systemctl restart zigbee2mqtt
 fi
