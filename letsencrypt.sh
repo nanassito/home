bdf0ac7ebd0293d40f9e4890563922545a4f9c85
Dorian JAMINAIS <724390+nanassito@users.noreply.github.com>
2022-02-01 22:05:26 -0800

    use volume to store tls certs

--- letsencrypt.sh
+++ letsencrypt.sh
@@ -1,12 +1,11 @@
 set -eux
 
-mkdir -p /github/home/letsencrypt
 sudo docker run \
     -it --rm \
     --name certbot \
     -p 80:80 \
-    --mount type=bind,source=/github/home/letsencrypt/etc,target=/etc/letsencrypt \
-    --mount type=bind,source=/github/home/letsencrypt/varlib,target=/var/lib/letencrypt \
+    --mount source=letsencrypt_etc,target=/etc/letsencrypt \
+    --mount source=letsencrypt_varlib,target=/var/lib/letencrypt \
     certbot/certbot \
     certonly \
     --manual \
