c38360770c733e83c3754e7d537477b3cb631b6f
Dorian Jaminais-Grellier <dorian@jaminais.fr>
2022-02-27 11:53:45 -0800

    enable irrigation in the frontyard

--- home/home/lawn.py
+++ home/home/lawn.py
@@ -44,9 +44,9 @@ class Irrigation:
         VALVE_BACKYARD_SCHOOL: Schedule(timedelta(minutes=5), timedelta(days=3)),
         VALVE_BACKYARD_HOUSE: Schedule(timedelta(minutes=5), timedelta(days=3)),
         VALVE_BACKYARD_DECK: Schedule(timedelta(minutes=5), timedelta(days=3)),
-        # VALVE_FRONTYARD_STREET: Schedule(timedelta(minutes=5), timedelta(days=3)),
-        # VALVE_FRONTYARD_DRIVEWAY: Schedule(timedelta(minutes=5), timedelta(days=3)),
-        # VALVE_FRONTYARD_NEIGHBOR: Schedule(timedelta(minutes=5), timedelta(days=3)),
+        VALVE_FRONTYARD_STREET: Schedule(timedelta(minutes=5), timedelta(days=3)),
+        VALVE_FRONTYARD_DRIVEWAY: Schedule(timedelta(minutes=5), timedelta(days=3)),
+        VALVE_FRONTYARD_NEIGHBOR: Schedule(timedelta(minutes=5), timedelta(days=3)),
         # VALVE_FRONTYARD_PLANTER: Schedule(timedelta(minutes=2), timedelta(days=1)),
     }
 
