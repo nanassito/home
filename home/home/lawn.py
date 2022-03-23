2400934ab162284e81afcc3ea8703d82e15cbf14
Dorian Jaminais-Grellier <dorian@jaminais.fr>
2022-03-22 19:06:53 -0700

    tweak irrigation settings

--- home/home/lawn.py
+++ home/home/lawn.py
@@ -53,17 +53,11 @@ class Irrigation:
         VALVE_BACKYARD_SCHOOL: Schedule(timedelta(minutes=5), timedelta(days=3)),
         VALVE_BACKYARD_HOUSE: Schedule(timedelta(minutes=5), timedelta(days=3)),
         VALVE_BACKYARD_DECK: Schedule(timedelta(minutes=5), timedelta(days=3)),
-        VALVE_FRONTYARD_STREET: Schedule(
-            timedelta(minutes=8), timedelta(days=1)
-        ),  # Should be 7m/3d
-        VALVE_FRONTYARD_DRIVEWAY: Schedule(
-            timedelta(minutes=5), timedelta(days=1)
-        ),  # Should be 7m/3d
-        VALVE_FRONTYARD_NEIGHBOR: Schedule(
-            timedelta(minutes=8), timedelta(days=1)
-        ),  # Should be 7m/3d
+        VALVE_FRONTYARD_STREET: Schedule(timedelta(minutes=8), timedelta(days=1)),
+        VALVE_FRONTYARD_DRIVEWAY: Schedule(timedelta(minutes=5), timedelta(days=3)),
+        VALVE_FRONTYARD_NEIGHBOR: Schedule(timedelta(minutes=8), timedelta(days=1)),
         VALVE_FRONTYARD_PLANTER: Schedule(
-            timedelta(minutes=8), timedelta(days=1), modifiers=set()
+            timedelta(minutes=15), timedelta(days=1), modifiers=set()
         ),
     }
 
