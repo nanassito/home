0d61a3162d199de8df641ba6a656c0720f71e2f5
Dorian Jaminais-Grellier <dorian@jaminais.fr>
2022-06-14 21:06:54 -0700

    add function to create a user certificate

---
+++ tls.sh
@@ -0,0 +1,4 @@
+create_user(){
+    USER=$1;
+    openssl genrsa -des3 -out ${USER}.key 4096 && openssl req -new -key ${USER}.key -out ${USER}.csr && openssl x509 -req -days 365 -in ${USER}.csr -CA ca.crt -CAkey ca.key -set_serial 01 -out ${USER}.crt && openssl pkcs12 -export -out ${USER}.pfx -inkey ${USER}.key -in ${USER}.crt -certfile ca.crt
+}
