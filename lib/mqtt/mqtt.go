b3b95d8962dd8bd4135937c847234b90de217e8d
Dorian Jaminais-Grellier <dorian@jaminais.fr>
2022-07-17 18:02:57 -0700

    Go | Fix mqtt connection bug

--- lib/mqtt/mqtt.go
+++ lib/mqtt/mqtt.go
@@ -2,12 +2,11 @@ package mqtt
 
 import (
 	"flag"
-	"net/url"
 
 	paho "github.com/eclipse/paho.mqtt.golang"
 )
 
-var server = flag.String("mqtt", "192.168.1.1:1883", "Address of the mqtt server.")
+var server = flag.String("mqtt", "tcp://192.168.1.1:1883", "Address of the mqtt server.")
 
 type MqttIface interface {
 	PublishString(topic string, message string) error
@@ -19,20 +18,14 @@ type Mqtt struct {
 
 func New(ClientID string) *Mqtt {
 	flag.Parse()
-	client := paho.NewClient(&paho.ClientOptions{
-		Servers: []*url.URL{
-			{
-				User: &url.Userinfo{},
-				Host: *server,
-			},
-		},
-		ClientID: ClientID,
-	})
+	client := paho.NewClient(paho.NewClientOptions().SetClientID(ClientID).AddBroker(*server))
+	if token := client.Connect(); token.Wait() && token.Error() != nil {
+		panic(token.Error())
+	}
 	return &Mqtt{client: client}
 }
 
 func (m *Mqtt) PublishString(topic string, message string) error {
-	m.client.Connect() // TODO: Mqtt failure: not Connected  ????
 	t := m.client.Publish(topic, 0, false, message)
 	<-t.Done()
 	return t.Error()
