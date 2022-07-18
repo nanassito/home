71ddb24aa9a166ee232a6b98ea5a97fafed54df6
Dorian Jaminais-Grellier <dorian@jaminais.fr>
2022-07-17 22:47:16 -0700

    app | use home app instead of nginx location for the music

--- home/home/web.py
+++ home/home/web.py
@@ -5,6 +5,11 @@ from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 
 WEB = FastAPI()
+WEB.mount(
+    "/static",
+    StaticFiles(directory=str(Path("__file__").parent / "static"), html=True),
+    name="static",
+)
 
 
 TEMPLATES = Jinja2Templates(directory=str(Path("__file__").parent / "templates"))
