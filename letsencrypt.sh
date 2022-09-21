b3d2dd1301009b1955e43003913123e149e83604
Dorian Jaminais-Grellier <dorian@jaminais.fr>
2022-09-20 21:42:19 -0700

    tls | improve renewal script

--- letsencrypt.sh
+++ letsencrypt.sh
@@ -1,14 +1,15 @@
 set -eux
 
-sudo docker run \
+docker run \
     -it --rm \
     --name certbot \
     -p 80:80 \
     --mount source=letsencrypt_etc,target=/etc/letsencrypt \
     --mount source=letsencrypt_varlib,target=/var/lib/letencrypt \
     certbot/certbot \
-    certonly \
+    # certonly \
+    renew \
     --manual \
-    -d "*.epa.jaminais.fr,*.eastpaloalto.jaminais.fr,epa.jaminais.fr,eastpaloalto.jaminais.fr" \
+    # -d "*.epa.jaminais.fr,*.eastpaloalto.jaminais.fr,epa.jaminais.fr,eastpaloalto.jaminais.fr" \
     --preferred-challenges=dns
 systemctl restart nginx
