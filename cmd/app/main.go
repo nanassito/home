9d36ad578275f90ddac8749cd632d6e997d8e47a
Dorian Jaminais-Grellier <dorian@jaminais.fr>
2023-01-11 22:54:28 -0800

    App | Add navbar

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -1,12 +1,21 @@
 package main
 
 import (
+	"flag"
 	"log"
 	"net/http"
 
 	"github.com/julienschmidt/httprouter"
 	"github.com/nanassito/home/pkg/app"
 )
+
+var (
+	staticRoot = flag.String("static-root", "/github/home/pkg/app/static/", "Root directory of where the static files are stored.")
+)
+
+func init() {
+	flag.Parse()
+}
 
 func main() {
 	server := app.NewServer()
@@ -16,8 +25,7 @@ func main() {
 	router.GET("/air/room/:roomID", server.GetRoom())
 	router.POST("/air/room/:roomID", server.PostRoom())
 
-	// fs := http.FileServer(http.Dir("static/"))
-	// http.Handle("/static/", http.StripPrefix("/static/", fs))
+	router.ServeFiles("/static/*filepath", http.Dir(*staticRoot))
 
 	log.Fatal(http.ListenAndServe(":7008", router))
 }
